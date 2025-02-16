9c502b960f79a5438bd8f59c7b1af876b497bbcb
akillez01 <achilles.oliveira.souza@gmail.com>
2025-02-15 19:22:32 -0500

    Atualizar config index

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// Configuração do Vite
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Caminho base para deploy, altere conforme necessário
   base: '/Page-Main/',
-  plugins: [
-    react(), // Plugin para suporte ao React
-  ],
-  server: {
-    open: true, // Abre o navegador automaticamente
-  },
+  plugins: [react()],
 });
